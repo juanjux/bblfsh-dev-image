53806d4f0933ada16fc6b872659a5d0174472de3
Juanjo Alvarez <juanjux@gmail.com>
2017-06-27 15:49:59 +0200

    Renamed test script to made it clearer that is a specific test

---
+++ test_server_issue34.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Example:
+# docker run -v $(pwd):/script --privileged -it --rm bblfshdev bash /script/test_server_issue34.sh
+
+GOPATH="/root/go"
+PATH="$GOPATH/bin:/usr/local/go/bin:$PATH"
+HOME="/root"
+
+# Docker run part starts here (no way to run privileged commands inside docker build)
+service docker start && \
+service docker status 
+
+if [ $? != 0 ]
+then
+    echo "Docker service not started. Did you started docker run with --privileged?"
+    exit 1
+fi
+
+# This also can't be done in the Dockerfile (same: no privileged operations on build) 
+echo "Building Python driver image..."
+cd $GOPATH/src/github.com/bblfsh/python-driver                    && \
+$GOPATH/bin/bblfsh-sdk prepare-build                              && \
+$GOPATH/bin/bblfsh-sdk update                                     && \ 
+make build # fails because root but creates the manifest.mk file
+echo "Ignore the error above... docker inside docker and permissions stuff... doesnt matter"
+chgrp -R docker $GOPATH/src/github.com/bblfsh/python-driver       && \
+chmod g+rwx -R $GOPATH/src/github.com/bblfsh/python-driver        && \
+su prebblfsh -c "make build" && \
+docker tag `docker images --quiet|head -1` bblfsh/python-driver:latest && \
+
+# Real testing starts here ~~~~~~~~~~~~
+
+echo "Build and run the server..."
+cd $GOPATH/src/github.com/bblfsh/server/cmd/bblfsh && \
+./bblfsh server --transport=docker-daemon&    
+sleep 2                                            && \
+
+echo "Cloning and installing specific client-python branch..."
+cd /root                                                          && \
+git clone https://github.com/vmarkovtsev/bblfsh.client-python.git && \
+cd /root/bblfsh.client-python                                     && \
+git checkout remotes/origin/main-change                           && \
+pip3 install -U .                                                 && \
+
+echo "Testing for 500 iterations with a sleep of 5 seconds"
+for i in `seq 1 500`;
+do
+    python3 -m bblfsh --disable-bblfsh-autorun bblfsh/github/com/bblfsh/sdk/__init__.py bblfsh/github/com/bblfsh/sdk/protocol/__init__.py  bblfsh/github/com/bblfsh/sdk/protocol/generated_pb2.py  bblfsh/github/com/bblfsh/sdk/protocol/generated_pb2_grpc.py  bblfsh/github/com/bblfsh/sdk/uast/__init__.py  bblfsh/github/com/bblfsh/sdk/uast/generated_pb2.py > 1.txt &
+    python3 -m bblfsh --disable-bblfsh-autorun bblfsh/github/com/bblfsh/sdk/__init__.py bblfsh/github/com/bblfsh/sdk/protocol/__init__.py  bblfsh/github/com/bblfsh/sdk/protocol/generated_pb2.py  bblfsh/github/com/bblfsh/sdk/protocol/generated_pb2_grpc.py  bblfsh/github/com/bblfsh/sdk/uast/__init__.py  bblfsh/github/com/bblfsh/sdk/uast/generated_pb2.py > 2.txt &
+    python3 -m bblfsh --disable-bblfsh-autorun bblfsh/github/com/bblfsh/sdk/__init__.py bblfsh/github/com/bblfsh/sdk/protocol/__init__.py  bblfsh/github/com/bblfsh/sdk/protocol/generated_pb2.py  bblfsh/github/com/bblfsh/sdk/protocol/generated_pb2_grpc.py  bblfsh/github/com/bblfsh/sdk/uast/__init__.py  bblfsh/github/com/bblfsh/sdk/uast/generated_pb2.py > 3.txt &
+    sleep 5
+    diff 1.txt 2.txt > /dev/null
+    D1=$?
+    diff 2.txt 3.txt > /dev/null
+    D2=$?
+    if [ "$D1" == 1 ] || [ "$D2" == 1 ]; then
+        echo "Different files!"
+        exit 1
+    fi
+    rm 1.txt 2.txt 3.txt
+    echo "Finished iteration $i"
+done
